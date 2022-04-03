d391d137edd3f6155fc813bf546375f31506013e
pransin <pranjal10singhal@gmail.com>
2022-04-03 17:13:25 +0530

    created basic login script

---
+++ loginscript.py
@@ -0,0 +1,59 @@
+# import subprocess
+import requests
+import sys
+import time
+import xml.etree.ElementTree as ET
+import os
+
+headers = {
+    'sec-ch-ua-mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.55',
+    'sec-ch-ua-platform': '"Windows"',
+    'Accept': '*/*',
+    'Origin': 'https://fw.bits-pilani.ac.in:8090',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://fw.bits-pilani.ac.in:8090/',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
+
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+with open(os.path.join(__location__, "creds.txt")) as creds:
+    name = creds.readline()
+    password = creds.readline()
+
+# Ineffective
+# try:
+#     with open(os.path.join(__location__, "warp-path.txt")) as warp_path:
+#         path = warp_path.readline().strip()
+#     subprocess.run(path + ' disconnect',
+#                    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+# except:
+#     pass
+
+data = {
+    'mode': '191',
+    'username': name.strip(),
+    'password': password.strip(),
+    'a': int(time.time()),
+    'producttype': '0',
+}
+try:
+    response = requests.post(
+        'https://fw.bits-pilani.ac.in:8090/login.xml', headers=headers, data=data)
+    if response.status_code == 200:
+        root = ET.fromstring(response.text)
+        result = root[1].text    # Extract message tag
+
+        if "failed" in result:
+            sys.exit(1)
+        sys.exit(0)
+    else:
+        sys.exit(100)
+except Exception as e:
+    print('Login Page Unreachable')
